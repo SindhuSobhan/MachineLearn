aa2e4306fd5c9dcb4a1e499a89af8a22e3e4d40d
SobhanChatterjee <sobhanchatterjee28@gmail.com>
2019-10-18 15:22:42 +0530

    Vectorised code for Optimised performance

--- Produced/KMeans.py
+++ Produced/KMeans.py
@@ -8,95 +8,103 @@ class KMeans:
     self.n_clusters = n_clusters
     
     
-  def initialise_parameters(self, data):
+  def initialise_general_parameters(self, data):
     (self.n, self.f) = data.shape
     random_indices = np.random.randint(low = 0, high = self.n, size = (1, self.n_clusters))
     self.centroids =  np.array(data[random_indices.squeeze(), :])
     self.cluster = np.ones(self.n).astype(np.int32) * 100
     self.costs = []
-    
-    
-    
-  def fit(self, data, max_iter = 20, recursive = False, n_recursion = 10, tol = 1e-6):
-    self.tol = tol
-    if not recursive:
-      self.general_kmeans_fit(data, max_iter)
-    else:
-      self.recursive_kmeans_fit(data, max_iter, n_recursion)
-    
-    
-      
-    
-  def general_kmeans_fit(self, data, max_iter):
-    self.initialise_parameters(data)
-    
-    for itr in range(max_iter):
-      self.forward_prop(data)
-      print("Cost for iteration " + str(itr + 1) + ": " + str(self.costs[-1]))
-      if self.stopping_criteria():
-        break
-      else:
-        self.update_param()
-        
-        
-        
-  def recursive_kmeans_fit(self, data, max_iter, n_recursion):
-    self.initialise_parameters(data)
-    
+
+
+  def initialise_recursive_parameters(self, n_recursion):
     self.rec_cost_best = []
     self.rec_centroids_best =  np.zeros((self.n_clusters, self.f))
     self.rec_clusters_best = np.ones(self.n).astype(np.int32) * 100
     
     
+
+  def fit(self, data, max_iter = 20, recursive = False, n_recursion = 10, tol = 1e-6):
+    self.tol = tol
+
+    if not recursive:
+        self.general_kmeans_fit(data, max_iter)
+        print("\n\n Final Cost:", self.costs[-1], sep = '\n')
+        print("\n\n Final Cluster Centroids:", self.centroids, sep = '\n')
+        if self.n < 100:
+            print("\n\n Final Cluster values:", self.cluster, sep = "\n")
+
+    else:
+        self.recursive_kmeans_fit(data, max_iter, n_recursion)
+        print("\n\n\n Best fit for:")
+        print("\n\nCost:", self.rec_cost_best, sep = '\n')
+        print("\n\nCentroids:" , self.rec_centroids_best, sep  = '\n')
+        if self.n < 100:
+            print("\n\nClusters:" , self.rec_clusters_best, sep  = '\n')
+    
+    
+      
+    
+  def general_kmeans_fit(self, data, max_iter):
+    self.initialise_general_parameters(data)
+    
+    for itr in range(max_iter):
+        self.forward_prop(data)
+        print("Cost for iteration " + str(itr + 1) + ": " + str(self.costs[-1]))
+        if self.stopping_criteria():
+            break
+        else:
+            self.update_param(data)
+    
+    
+        
+        
+        
+  def recursive_kmeans_fit(self, data, max_iter, n_recursion):
+    self.initialise_general_parameters(data)
+    self.initialise_recursive_parameters(n_recursion)
+
     rec_clusters = np.zeros((n_recursion, self.n))
     rec_centroids = np.zeros((n_recursion, self.n_clusters, self.f))
-    self.rec_cost = np.zeros(n_recursion)
+    rec_cost = np.zeros(n_recursion)
     
     for recur in range(n_recursion):
-      print("Recursion ", str(recur + 1), ":")
-      self.general_kmeans_fit(data, max_iter)
-      rec_clusters[recur] = self.cluster
-      rec_centroids[recur, ...] = self.centroids
-      self.rec_cost[recur] = (self.costs[-1])
+        print("Recursion ", str(recur + 1), ":")
+        self.general_kmeans_fit(data, max_iter)
+        rec_clusters[recur] = self.cluster
+        rec_centroids[recur, ...] = self.centroids
+        rec_cost[recur] = (self.costs[-1])
     
-    ind = np.argmin(self.rec_cost)
+    ind = np.argmin(rec_cost)
     
     self.rec_centroids_best = rec_centroids[ind, ...]
     self.rec_clusters_best = rec_clusters[ind, :]
-    self.rec_cost_best = self.rec_cost[ind]
-    
-    
-    print("\n\n\n Best fit for:")
-    print("\n\nCost:", self.rec_cost_best, sep = '\n')
-    print("\n\nCentroids:" , self.rec_centroids_best, sep  = '\n')
-    print("\n\nClusters:" , self.rec_clusters_best, sep  = '\n')
+    self.rec_cost_best = rec_cost[ind]
       
   
   
   
   def forward_prop(self, data):
-    cost = 0
-    dist = np.zeros(self.n_clusters)
+    cost = np.zeros(self.n_clusters)
+    dist = np.zeros((self.n, self.n_clusters))
     
-    for i in range(self.n):
-      pt = data[i,:]
-      for j, c in enumerate(self.centroids):
-        dist[j] = np.linalg.norm(pt - c + self.tol, 2)
-      
-        self.cluster[i] = np.argmin(dist).astype(np.int32)
-      cost = cost + np.square(np.linalg.norm(pt - self.centroids[self.cluster[i]] + self.tol, 2))
-        
-    self.costs.append(cost)
-      
+    for j, c in enumerate(self.centroids):
+        dist[:, j] = np.square(np.linalg.norm(data - c , axis = 1))
+
+    self.cluster = np.argmin(dist, axis = 1).astype(np.int32)
+
+    for j, c in enumerate(self.centroids):
+        cost[j] = np.sum(np.linalg.norm(data[self.cluster == j] - c , axis = 1) ** 2) 
+    self.costs.append(np.sum(cost))
       
       
-  def update_param(self):
+      
+  def update_param(self, data):
     if len(np.unique(self.cluster)) < self.n_clusters:
-        print("Cluster initialisation inappropriate, Reinitialising..")
-        self.initialise_parameters(data)
+        print("Cluster initialisation inadequate, Reinitialising..")
+        self.initialise_general_parameters(data)
     else:
-      for c in range(self.n_clusters):
-        self.centroids[c] = np.mean(data[self.cluster == c, :], axis = 0)
+        for c in range(self.n_clusters):
+            self.centroids[c] = np.mean(data[self.cluster == c, :], axis = 0)
           
           
           
@@ -108,13 +116,13 @@ class KMeans:
 
 
 if __name__ == '__main__':
-    dataset = make_blobs(n_samples = 1000, n_features =2, centers = 10)
+    dataset = make_blobs(n_samples = 10000, n_features =2, centers = 5)
 
     data = dataset[0]
-    cl = KMeans(10)
+    cl = KMeans(5)
     cl.fit(data, recursive = True)
 
-    plt.figure(figsize = (20, 10))
+    plt.figure(figsize = (10, 5))
     plt.subplot(1,2,1)
     for i in range(len(np.unique(dataset[1]))):
         plt.scatter(dataset[0][dataset[1] == i,0], dataset[0][dataset[1] == i, 1])
@@ -122,8 +130,8 @@ if __name__ == '__main__':
 
     plt.subplot(1,2,2)
     for i in range(len(np.unique(dataset[1]))):
-      plt.scatter(dataset[0][cl.rec_clusters_best == i, 0], dataset[0][cl.rec_clusters_best == i, 1])
-      plt.title('K-Means Clusters')
+        plt.scatter(dataset[0][cl.rec_clusters_best == i, 0], dataset[0][cl.rec_clusters_best == i, 1])
+        plt.title('K-Means Clusters')
     
     plt.tight_layout()  
     plt.show()
